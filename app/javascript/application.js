3abd3b68b16f5a4e577f74173c9b8ff422a52c26
Bryan Feller <brizzlefeller@gmail.com>
2023-04-20 01:12:17 -0400

    force sprockets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "bootstrap"
+import "jquery"
+import "./main"
+
+
+import "trix"
+import "@rails/actiontext"
+import "controllers"
